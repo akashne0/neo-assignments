27597878bed38d52a0f05f9611e5e893bc31ebcc
Akash <akash.singh@neosoftmail.com>
2022-03-20 17:32:16 +0530

    naming updated

--- javascript/assign2-16032022.js
+++ javascript/assign2-16032022.js
@@ -15,119 +15,132 @@ window.onload = function () {
 };
 
 function validation() {
-    valid = true;
+
+    validationFlag = true;
+    
     // for first name validation
-    var fname = document.getElementById("fName").value;
-    if (fname == ""){
-        document.getElementById("fName-error").style.display = "block";
-        document.getElementById("fName-error").innerHTML = "**Please enter your first name";
-        valid = false;
+    var firstName = document.getElementById("txt-firstname").value;
+    if (firstName == ""){
+        document.getElementById("sp-firstname-error").style.display = "block";
+        document.getElementById("sp-firstname-error").innerHTML = "**Please enter your first name";
+        validationFlag = false;
+
     }else{
-        document.getElementById("fName-error").style.display = "none";
-        document.getElementById("fName-error").innerHTML = "";
+        document.getElementById("sp-firstame-error").style.display = "none";
+        document.getElementById("sp-firstname-error").innerHTML = "";
     }
 
     // for last name validation
-    var lname = document.getElementById("lName").value;
-    if (lname == ""){
+    var lastname = document.getElementById("txt-lastname").value;
+    if (lastname == ""){
         // alert("Last name must be filled out")
-        document.getElementById("lName-error").style.display = "block";
-        document.getElementById("lName-error").innerHTML = "**Please enter your last name";
-       valid = false;
+        document.getElementById("sp-lastname-error").style.display = "block";
+        document.getElementById("sp-lastname-error").innerHTML = "**Please enter your last name";
+        validationFlag = false;
+
     }else{
-        document.getElementById("lName-error").style.display = "none";
-        document.getElementById("lName-error").innerHTML = "";
+        document.getElementById("sp-lastname-error").style.display = "none";
+        document.getElementById("sp-lastname-error").innerHTML = "";
 
     }
 
     // for phone number validation
-    var phone = document.getElementById("phone").value;
+    var phone = document.getElementById("txt-phone").value;
     // // if (phn == (1 > phn.length) || (10 < phn.length)){ 
     //     // if (phn.length !== 10)  {
     // number empty or not validation
     if (phone == ""){
-        document.getElementById("phone-error").style.display = "block";
-        document.getElementById("phone-error").innerHTML = " **Number cannot be blank ";
-        valid = false;
+        document.getElementById("sp-phone-error").style.display = "block";
+        document.getElementById("sp-phone-error").innerHTML = " **Number cannot be blank ";
+        validationFlag = false;
+
     }else if(phone.length != 10){
-        document.getElementById("phone-error").style.display = "block";
-        document.getElementById("phone-error").innerHTML = "**Please enter valid phone number";
-        valid = false;
+        document.getElementById("sp-phone-error").style.display = "block";
+        document.getElementById("sp-phone-error").innerHTML = "**Please enter valid phone number";
+        validationFlag = false;
+
     }else if (isNaN(phone)) {
-        document.getElementById("phone-error").style.display = "block";
-        document.getElementById("phone-error").innerHTML = "**Please enter only number";
-       valid = false;
+        document.getElementById("sp-phone-error").style.display = "block";
+        document.getElementById("sp-phone-error").innerHTML = "**Please enter only number";
+        validationFlag = false;
+
     }else{
-        document.getElementById("phone-error").style.display = "none";
-        document.getElementById("phone-error").innerHTML = "";
+        document.getElementById("sp-phone-error").style.display = "none";
+        document.getElementById("sp-phone-error").innerHTML = "";
     }
 
     
-    var poffice = document.getElementById("pOffice").value;
+    var phoneNumberOffice = document.getElementById("txt-phone-number-office").value;
     // office number (only number no other char) validation;
-    if (isNaN(poffice)) {
-        document.getElementById("pOffice-error").style.display= "block";
-        document.getElementById("pOffice-error").innerHTML= "**Please enter only number";
-       valid = false;
+    if (isNaN(phoneNumberOffice)) {
+        document.getElementById("sp-phone-number-offic-error").style.display= "block";
+        document.getElementById("sp-phone-number-offic-error").innerHTML= "**Please enter only number";
+        validationFlag = false;
+
     }else{
-        document.getElementById("pOffice-error").style.display= "none";
-        document.getElementById("pOffice-error").innerHTML= "";
+        document.getElementById("sp-phone-number-offic-error").style.display= "none";
+        document.getElementById("sp-phone-number-offic-error").innerHTML= "";
     }
     
-    var email = document.getElementById("email").value;
+    var email = document.getElementById("txt-email").value;
     // email id (empty or not) validation
     if (email == ""){
-        document.getElementById("email-error").style.display= "block";
-        document.getElementById("email-error").innerHTML= "**Email cannot be blank";
-       valid = false;
+        document.getElementById("sp-email-error").style.display= "block";
+        document.getElementById("sp-email-error").innerHTML= "**Email cannot be blank";
+        validationFlag = false;
+
     }else{
-        document.getElementById("email-error").style.display= "none";
-        document.getElementById("email-error").innerHTML= "";
+        document.getElementById("sp-email-error").style.display= "none";
+        document.getElementById("sp-email-error").innerHTML= "";
     }
 
     // email id (empty or not)format validation
     // var re = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8}+)(\.[a-z]{2,8})?$/;
-    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (!re.test(email)){
-        document.getElementById("email-error").style.display= "block";
-        document.getElementById("email-error").innerHTML= "**Email format is not valid";
-       valid = false;
+    var regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!regEmail.test(email)){
+        document.getElementById("sp-email-error").style.display= "block";
+        document.getElementById("sp-email-error").innerHTML= "**Email format is not valid";
+        validationFlag = false;
+
     }else{
-        document.getElementById("email-error").style.display= "none";
-        document.getElementById("email-error").innerHTML= "";
+        document.getElementById("sp-email-error").style.display= "none";
+        document.getElementById("sp-email-error").innerHTML= "";
     }
     
     
-    var pass = document.getElementById("pass").value;
-        // password validation
-    if (pass == "") {
-        document.getElementById("pass-error").innerHTML= "**password cannot be blank";
-       valid = false;  
+    var password = document.getElementById("txt-password").value;
+    // password validation
+    if (password == "") {
+        document.getElementById("sp-password-error").innerHTML= "**password cannot be blank";
+        validationFlag = false;
+
     }else{
-        document.getElementById("pass-error").style.display= "none";
-        document.getElementById("pass-error").innerHTML= "";
+        document.getElementById("sp-password-error").style.display= "none";
+        document.getElementById("sp-password-error").innerHTML= "";
     }
 
-        // alphanumeric validation;
-    var rege = /^\w{8,12}$/;
-    if (!rege.test(pass)){
-        document.getElementById("pass-error").style.display= "block";
-        document.getElementById("pass-error").innerHTML= "**Password should be alpha-numeric and between 8-12 characters";
-       valid = false;
+    // alphanumeric validation;
+    var regPassword = /^\w{8,12}$/;
+    if (!regPassword.test(pass)){
+        document.getElementById("sp-password-error").style.display= "block";
+        document.getElementById("sp-password-error").innerHTML= "**Password should be alpha-numeric and between 8-12 characters";
+        validationFlag = false;
+
     }else{
-        document.getElementById("pass-error").style.display= "none";
-        document.getElementById("pass-error").innerHTML= "";
+        document.getElementById("sp-password-error").style.display= "none";
+        document.getElementById("sp-password-error").innerHTML= "";
     }
 
-    var conpass = document.getElementById("conpass").value;
-        // match password
-    if (conpass != pass ) {
-        document.getElementById("conpass-error").style.display= "block";
-        document.getElementById("conpass-error").innerHTML= "**password do not match";
-       valid = false;  
+    var confirmPassword = document.getElementById("txt-confirm-password").value;
+    // match password
+    if (confirmPassword != password ) {
+        document.getElementById("sp-confirm-password-error").style.display= "block";
+        document.getElementById("sp-confirm-password-error").innerHTML= "**password do not match";
+        validationFlag = false;
+
     }else{
-        document.getElementById("conpass-error").style.display= "none";
-        document.getElementById("conpass-error").innerHTML= "";
+        document.getElementById("sp-confirm-password-error").style.display= "none";
+        document.getElementById("sp-confirm-password-error").innerHTML= "";
     }
 
     // DOB validation
@@ -149,58 +162,63 @@ function validation() {
     // }
 
     if (day == "day"){
-        document.getElementById("date-error").style.display= "block";
-        document.getElementById("date-error").innerHTML= "please select proper date format";
-        valid = false;
+        document.getElementById("sp-date-error").style.display= "block";
+        document.getElementById("sp-date-error").innerHTML= "please select proper date format";
+        validationFlag = false;
+
     }else if (month == "month"){
-        document.getElementById("date-error").style.display= "block";
-        document.getElementById("date-error").innerHTML= "please select proper date format";
-        valid = false;
+        document.getElementById("sp-date-error").style.display= "block";
+        document.getElementById("sp-date-error").innerHTML= "please select proper date format";
+        validationFlag = false;
+
     }else if(year == "year" ){
-        document.getElementById("date-error").style.display= "block";
-        document.getElementById("date-error").innerHTML= "please select proper date format";
-        valid = false;
+        document.getElementById("sp-date-error").style.display= "block";
+        document.getElementById("sp-date-error").innerHTML= "please select proper date format";
+        validationFlag = false;
+
     }else{
-        document.getElementById("date-error").style.display= "none";
-        document.getElementById("date-error").innerHTML= "";
+        document.getElementById("sp-date-error").style.display= "none";
+        document.getElementById("sp-date-error").innerHTML= "";
     }
 
     // gender validation
     gender = document.getElementsByName("rdb-gender");
     if (!(gender[0].checked || gender[1].checked)) {
-        document.getElementById("gender-error").style.display = "block";
-        document.getElementById("gender-error").innerHTML = "**please select atleast 1 value";
-            valid = false; 
+        document.getElementById("sp-gender-error").style.display = "block";
+        document.getElementById("sp-gender-error").innerHTML = "**please select atleast 1 value";
+        validationFlag = false;
+
     }else{
-        document.getElementById("gender-error").style.display = "none";
-        document.getElementById("gender-error").innerHTML = "";
+        document.getElementById("sp-gender-error").style.display = "none";
+        document.getElementById("sp-gender-error").innerHTML = "";
 
     }
     
     // interest validation
-    var checkboxActivity = document.querySelectorAll('input[name="rdb-gender"]:checked').length;
+    var checkboxActivity = document.querySelectorAll('input[name="chkbx-activity"]:checked').length;
     if (checkboxActivity < 2){
-        document.getElementById("interest-error").style.display = "block";
-        document.getElementById("interest-error").innerHTML = "**please select atleast 1 value";
-        valid = false; 
+        document.getElementById("sp-interest-error").style.display = "block";
+        document.getElementById("sp-interest-error").innerHTML = "**please select atleast 1 value";
+        validationFlag = false;
+
     }else{
-        document.getElementById("interest-error").style.display = "none";
-        document.getElementById("interest-error").innerHTML = "";
+        document.getElementById("sp-interest-error").style.display = "none";
+        document.getElementById("sp-interest-error").innerHTML = "";
     }
 
     // textbox validation
-    var text = document.getElementById("about").value;
+    var text = document.getElementById("txt-about").value;
     if (text == "" ){
-        document.getElementById("about-error").style.display = "block";
-        document.getElementById("about-error").innerHTML = " **please enter some text ";
-            valid = false;
+        document.getElementById("sp-about-error").style.display = "block";
+        document.getElementById("sp-about-error").innerHTML = " **please enter some text ";
+        validationFlag = false;
+
     }else{
-        document.getElementById("about-error").style.display = "none";
-        document.getElementById("about-error").innerHTML = "";
+        document.getElementById("sp-about-error").style.display = "none";
+        document.getElementById("sp-about-error").innerHTML = "";
     }
-    
-    return valid;  
-}
+    return validationFlag;  
+};
 
 
 function calculateAge(){
@@ -218,9 +236,7 @@ function calculateAge(){
         var age = currentYear - birthYear;
         
         // alert(age);
-
         // var msg = "You are " + age + " years old";
         document.getElementById("txt-age").value = age;
-
     }
-}
+};
